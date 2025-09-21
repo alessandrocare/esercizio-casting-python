c6fe4b5d4ea513817c0cbcd1884cfa9b89fe378c
alessandrocare <alessandrocareglio2003@gmail.com>
2025-09-21 16:36:33 +0200

    Add files via upload

---
+++ casting.py
@@ -0,0 +1,4 @@
+numero_di_telefono = int(input("Inserisci il tuo numero di telefono"))
+numero_decimale = float(numero_di_telefono)
+numero_in_stringa = str(numero_di_telefono)
+print("I tuoi numeri convertiti sono:", numero_decimale, numero_in_stringa)
